ee22ca54452931655da906d56d40f79aa833821f
Nishant Kumar <nsingh9430@gmail.com>
2026-06-24 22:59:31 +0530

    0540-single-element-in-a-sorted-array .cpp Medium

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,15 +2,26 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         //o(n) o(n)
-        unordered_map<int,int>mp;
-        for(int i: nums){
-            mp[i]++;
-        }
-        for(auto i: mp){
-            if(i.second==1) return i.first;
-        }
-        return -1;
+        // unordered_map<int,int>mp;
+        // for(int i: nums){
+        //     mp[i]++;
+        // }
+        // for(auto i: mp){
+        //     if(i.second==1) return i.first;
+        // }
+        // return -1;
 
 
+        //o(n) o(1)
+
+
+        int ans=0;
+        for(int i : nums){
+            ans^=i;
+        }
+        return ans;
+
+
+        
     }
 };
