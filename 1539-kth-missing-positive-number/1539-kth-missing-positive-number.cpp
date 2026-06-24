e18e81ebdaa767409a17b8c1e8f688c04d3d56c0
Nishant Kumar <nsingh9430@gmail.com>
2026-06-24 20:13:43 +0530

    1539-kth-missing-positive-number .cpp Easy

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-       priority_queue<int, vector<int>, greater<int>> pq;
-       unordered_set<int>st(arr.begin(),arr.end());
-       int maxi=*max_element(arr.begin(),arr.end());
-       for(int i=1;i<=maxi+k;i++){
-        if(st.find(i)==st.end()){
-            pq.push(i);
+    //    priority_queue<int, vector<int>, greater<int>> pq;
+    //    unordered_set<int>st(arr.begin(),arr.end());
+    //    int maxi=*max_element(arr.begin(),arr.end());
+    //    for(int i=1;i<=maxi+k;i++){
+    //     if(st.find(i)==st.end()){
+    //         pq.push(i);
+    //     }
+    //    }
+    //    for(int i=1;i<k;i++){
+    //     pq.pop();
+    //    }
+    //    return pq.top();
+
+
+    // o(logN)
+    int low=0,high=arr.size()-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(arr[mid] - (mid + 1) >= k){
+            high=mid-1;
         }
-       }
-       for(int i=1;i<k;i++){
-        pq.pop();
-       }
-       return pq.top();
+        else{
+            low=mid+1;
+        }
+    }
+    return low+k;
+    
     }
 };
