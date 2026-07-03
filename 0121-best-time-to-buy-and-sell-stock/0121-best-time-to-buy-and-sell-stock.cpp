80fbb2752f202ad49a2320aa125f6f82c37369fb
Nishant Kumar <nsingh9430@gmail.com>
2026-07-03 19:41:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& nums) {
+        //o(n2)
+        int n=nums.size();
+        if(n<2){
+            return 0;
+        }
+        int l=0,r=1;
+        int ans=0;
+        while(r<n){
+            if(nums[l]<=nums[r]){
+                ans=max(ans,nums[r]-nums[l]);
+            }
+            else{
+                l++;
+            }
+            r++;
+        }
+        return ans;
+    }
+};
