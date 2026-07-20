463f9ec2087055f57f31c425941c0ed924aadcdd
Nishant Kumar <nsingh9430@gmail.com>
2026-07-20 23:13:36 +0530

    Time: 2 ms (81.66%), Space: 13 MB (90.3%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-   void    tryCombinations(int open, int close, vector<string>& res, string str)
-{
-        if (open == 0 && close == 0)
-        {
+    void tryCombinations(int open, int close, vector<string>& res, string &str) {
+        if (open == 0 && close == 0) {
             res.push_back(str);
-            return ;
+            return;
         }
-        if (open > 0)
-            tryCombinations(open - 1, close, res, str + "(");
-        if (close > 0 && close > open)
-            tryCombinations(open, close - 1, res, str + ")");
-}
 
-vector<string> generateParenthesis(int n)
-{
-        vector<string>  res;
+        if (open > 0) {
+            str.push_back('(');
+            tryCombinations(open - 1, close, res, str);
+            str.pop_back();
+        }
 
-        tryCombinations(n, n, res, "");
+        if (close > open) {
+            str.push_back(')');
+            tryCombinations(open, close - 1, res, str);
+            str.pop_back();
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        string str;
+        tryCombinations(n, n, res, str);
         return res;
-}
+    }
 };
