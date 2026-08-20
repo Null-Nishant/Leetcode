f13e849012a30ac9f0e53935e82d8acab43f3882
Nishant Kumar <nsingh9430@gmail.com>
2026-08-20 15:14:47 +0530

    Time: 0 ms (100%), Space: 8.5 MB (69.34%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        string s1 = "";
+        string s2 = "";
+        for (int i = 0; i < s.size(); i++) {
+            if (!s1.empty() && s[i] == '#') {
+                s1.pop_back();
+                continue;
+            } else if (s[i] != '#') {
+                s1.push_back(s[i]);
+            }
+        }
+
+        for (int i = 0; i < t.size(); i++) {
+            if (!s2.empty() && t[i] == '#') {
+                s2.pop_back();
+                continue;
+            } else if (t[i] != '#') {
+                s2.push_back(t[i]);
+            }
+        }
+        return s1 == s2;
+    }
+};
