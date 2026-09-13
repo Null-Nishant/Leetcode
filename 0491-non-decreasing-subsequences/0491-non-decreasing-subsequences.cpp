155f4d516e568ff6a1a3098ea64dac4731cb9ce5
Nishant Kumar <nsingh9430@gmail.com>
2026-09-13 19:06:38 +0530

    Time: 19 ms (93.76%), Space: 26.4 MB (71.13%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -1,46 +1,29 @@
 class Solution {
 public:
-
-    void solver(int index,
-                vector<int>& nums,
-                vector<int>& path,
-                vector<vector<int>>& ans) {
-
-        if (path.size() >= 2) {
+    void solver(int index, vector<vector<int>>& ans, vector<int>& path, vector<int>& nums) {
+        if (path.size() > 1) {
             ans.push_back(path);
         }
-
+        
         unordered_set<int> used;
-
         for (int i = index; i < nums.size(); i++) {
-
-            // same recursion level duplicate
-            if (used.count(nums[i])) {
-                continue;
-            }
-
-            // maintain non-decreasing order
-            if (!path.empty() && nums[i] < path.back()) {
-                continue;
-            }
-
+            // Skip duplicates at the same recursion level
+            if (used.count(nums[i])) continue;
+            
+            // Ensure non-decreasing order
+            if (!path.empty() && path.back() > nums[i]) continue;
+            
             used.insert(nums[i]);
-
             path.push_back(nums[i]);
-
-            solver(i + 1, nums, path, ans);
-
+            solver(i + 1, ans, path, nums);
             path.pop_back();
         }
     }
-
+    
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-
-        vector<vector<int>> ans;
         vector<int> path;
-
-        solver(0, nums, path, ans);
-
+        vector<vector<int>> ans;
+        solver(0, ans, path, nums);
         return ans;
     }
 };
